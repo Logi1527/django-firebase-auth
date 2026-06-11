eed968a70dfb1ba355e19bc8f9f3fdde56f2012f
Logi1527 <9830logaesh2122@gmail.com>
2026-06-12 00:34:40 +0530

    Clean up comments in urls.py
    Removed commented line explaining the URL mapping.

--- firebase_auth/urls.py
+++ firebase_auth/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import VerifyUserView
 
 urlpatterns = [
-    # This connects the '/verify/' URL to the View we built in Step 4
     path('verify/', VerifyUserView.as_view(), name='firebase-verify'),
 ]

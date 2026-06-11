7636f4445a5bb9aa439b653a15db9ab8c581db1a
Logi1527 <9830logaesh2122@gmail.com>
2026-06-12 00:35:12 +0530

    Refactor VerifyUserView authentication comments

--- firebase_auth/views.py
+++ firebase_auth/views.py
@@ -4,15 +4,13 @@ from rest_framework.permissions import IsAuthenticated
 from .authentication import FirebaseAuthentication
 
 class VerifyUserView(APIView):
-    # 1. Assign our custom Firebase security guard to this specific view
     authentication_classes = [FirebaseAuthentication]
     
-    # 2. Lock the door: strictly require the user to be authenticated
+  
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        # 3. If the code reaches this point, the token was valid!
-        # request.user was automatically populated by our authentication.py file
+       
         return Response({
             "status": "success",
             "message": "Successfully authenticated with Firebase!",
